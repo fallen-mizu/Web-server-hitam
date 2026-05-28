0ec4153199eccc6b6423c59dfe3617056ae1c88d
fallen-mizu <tohsakarin756@gmail.com>
2026-05-29 02:31:52 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         const { image } = req.body;
 
         const response = await groq.chat.completions.create({
-            model: "llama-3.2-11b-vision-preview",
+            model: "meta-llama/llama-4-scout-17b-16e-instruct",
             messages: [
                 {
                     role: "user",
